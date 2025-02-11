07c8bd6c2bbaa4f94be0f738021666583d3f8ba6
Bernard Maximiliano <bmaximiliano0210@gmail.com>
2025-02-11 15:32:30 -0300

    style: improve css

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -3,14 +3,20 @@ import { useState } from "react";
 function Tasks() {
   const [tasks, setTasks] = useState<string[]>([]);
   const [input, setInput] = useState<string>("");
+  const addNewTask = () => {
+    if (input === "") return;
+    setTasks([...tasks, input]);
+    setInput("");
+  }
   return (
     <div>
-      <h1>Tasks</h1>
-      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
-      <button onClick={() => setTasks([...tasks, input])}>Add Task</button>
+      <h1>Tasks</h1><div style={{ display: "flex",justifyContent:"center", gap: "10px", paddingBottom: "10px" }}>
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+        <button onClick={() => addNewTask()}>Add Task</button>
+      </div>
       <ul>
         {tasks.map((task, index) => (
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div key={index} style={{ display: "flex", justifyContent: "left", alignItems: "center", gap: "10px" }}>
             <li key={index}>{task}</li>
             <button onClick={() => setTasks(tasks.filter((_, i) => i !== index))}>Remove</button>
           </div>
